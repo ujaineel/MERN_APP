8a68c56155d1c1d2067037d3e82199fe11fea40f
Jaineel Upadhyay <ujaineel@gmail.com>
2021-12-30 17:44:57 -0500

    Input added. Validation issue

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+
+import Input from '../components/Input';
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import "./NewPlace.css";
 
 const NewPlace = () => {
     return (
-        <h2>NewPlace Works!</h2>
-    )
+        <form className="place-form">
+            <Input 
+                type="text" 
+                label="Title" 
+                validators={[VALIDATOR_REQUIRE]} 
+                errorText="Please enter a valid title" 
+                element="input"
+            />
+        </form>
+    );
 }
 
-export default NewPlace
+export default NewPlace;
